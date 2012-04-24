eaca605148f2b587c2e8f141114d80bed901197f
Frank A. Krueger <fak@praeclarum.org>
2012-04-24 15:11:52 -0700

    Added Android sample

---
+++ samples/Clock/ClockAndroid/Activity1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Timers;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Views;
+using CrossGraphics.Droid;
+
+namespace Clock.Android
+{
+	[Activity (Label = "ClockAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class Activity1 : Activity
+	{
+		ClockView _view;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			_view = new ClockView (this);
+			SetContentView (_view);
+
+			var timer = new Timer (1);
+			timer.Elapsed += delegate {
+				RunOnUiThread (delegate {
+					_view.Invalidate ();
+				});
+			};
+			timer.Start ();
+		}
+
+		class ClockView : View
+		{
+			Clock _clock = new Clock ();
+
+			public ClockView (Context context)
+				: base (context)
+			{
+			}
+
+			public override void Draw (global::Android.Graphics.Canvas canvas)
+			{
+				base.Draw (canvas);
+
+				var g = new DroidGraphics (canvas);
+
+				_clock.Width = Width;
+				_clock.Height = Height;
+
+				_clock.Draw (g);
+			}
+		}
+	}
+}
