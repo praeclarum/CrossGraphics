7951f01a2b57bcc66746c5b95fc0ef4054e0569f
Frank A. Krueger <fak@praeclarum.org>
2012-04-24 17:49:12 -0700

    Added WPF Clock sample

---
+++ samples/Clock/ClockWpf/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using CrossGraphics.SilverlightGraphics;
+
+namespace ClockWpf
+{
+	public partial class MainWindow : Window
+	{
+		Clock.Clock _clock;
+		SilverlightGraphics _graphics;
+
+		public MainWindow ()
+		{
+			InitializeComponent ();
+			_clock = new Clock.Clock ();
+		}
+
+		private void Window_Loaded (object sender, RoutedEventArgs e)
+		{
+			_graphics = new SilverlightGraphics (LayoutRoot);
+
+			var timer = new DispatcherTimer {
+				Interval = TimeSpan.FromSeconds (1),
+			};
+			timer.Tick += delegate {
+				Draw ();
+			};
+			timer.Start ();
+		}
+
+		void Draw ()
+		{
+			_graphics.BeginDrawing ();
+
+			_clock.Width = (float)LayoutRoot.ActualWidth;
+			_clock.Height = (float)LayoutRoot.ActualHeight;
+			_clock.Draw (_graphics);
+
+			_graphics.EndDrawing ();
+		}
+	}
+}
