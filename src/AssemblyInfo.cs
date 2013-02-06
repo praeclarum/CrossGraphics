7311cedc69c573c58dfd12784af9a34db9aaae73
Frank A. Krueger <fak@praeclarum.org>
2013-02-06 14:11:19 -0800

    Finally wrote an AssemblyInfo.cs

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CrossGraphics")]
+[assembly: AssemblyDescription("Cross-platform library for rendering vector graphics.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Krueger Systems, Inc.")]
+[assembly: AssemblyProduct("CrossGraphics")]
+[assembly: AssemblyCopyright("Copyright © 2010-2013 Krueger Systems, Inc.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: ComVisible(false)]
