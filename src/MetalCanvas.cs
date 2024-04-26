580ef8459a90a6cf77d64ee7555f2e682247acfa
Frank A. Krueger <fak@praeclarum.org>
2024-04-26 15:35:11 -0700

    Start work on MetalCanvas

---
+++ src/MetalCanvas.cs
@@ -0,0 +1,84 @@
+#nullable enable
+
+//
+// Copyright (c) 2024 Frank A. Krueger
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+using Metal;
+
+using MetalKit;
+
+namespace CrossGraphics.Metal
+{
+	[Foundation.Register ("MetalCanvas")]
+	public class MetalCanvas : MTKView
+	{
+		public readonly IMTLDevice? CanvasDevice = MTLDevice.SystemDefault;
+		public MetalCanvas (IntPtr handle) : base (handle)
+		{
+			Initialize ();
+		}
+		public MetalCanvas () : base ()
+		{
+			Initialize ();
+		}
+		void Initialize ()
+		{
+			Device = CanvasDevice;
+			ClearColor = new MTLClearColor (0.5, 0, 0.75, 1);
+			AutoResizeDrawable = true;
+			PreferredFramesPerSecond = 30;
+			FramebufferOnly = true;
+			PresentsWithTransaction = false;
+			Paused = false;
+			Delegate = new MetalCanvasDelegate ();
+		}
+	}
+
+	public class MetalCanvasDelegate : MTKViewDelegate
+	{
+		public readonly IMTLCommandQueue? CommandQueue = MTLDevice.SystemDefault?.CreateCommandQueue ();
+		public MetalCanvasDelegate ()
+		{
+		}
+		public override void DrawableSizeWillChange (MTKView view, global::CoreGraphics.CGSize size)
+		{
+		}
+		public override void Draw (MTKView view)
+		{
+			if (view.Device is not { } device ||
+			    view.CurrentRenderPassDescriptor is not { } renderPassDescriptor ||
+			    view.CurrentDrawable is not { } drawable) {
+				return;
+			}
+			using var commandBuffer = CommandQueue?.CommandBuffer ();
+			if (commandBuffer is not null) {
+				using var renderEncoder = commandBuffer.CreateRenderCommandEncoder (renderPassDescriptor);
+				renderEncoder.EndEncoding ();
+				commandBuffer.PresentDrawable (drawable);
+				commandBuffer.Commit ();
+			}
+		}
+	}
+}
