00eafa32a0c24f6399407766ed83eabfbb4c5667
Frank A. Krueger <fak@praeclarum.org>
2024-04-26 20:47:36 -0700

    Add draw ops

--- src/MetalGraphics.cs
+++ src/MetalGraphics.cs
@@ -112,6 +112,15 @@ namespace CrossGraphics.Metal
 			vdesc.Attributes[2] = new MTLVertexAttributeDescriptor {
 				BufferIndex = 0, Format = MTLVertexFormat.Float4, Offset = 4 * sizeof (float),
 			};
+			vdesc.Attributes[3] = new MTLVertexAttributeDescriptor {
+				BufferIndex = 0, Format = MTLVertexFormat.Float4, Offset = 8 * sizeof (float),
+			};
+			vdesc.Attributes[4] = new MTLVertexAttributeDescriptor {
+				BufferIndex = 0, Format = MTLVertexFormat.Float4, Offset = 12 * sizeof (float),
+			};
+			vdesc.Attributes[5] = new MTLVertexAttributeDescriptor {
+				BufferIndex = 0, Format = MTLVertexFormat.UInt, Offset = 16 * sizeof (float),
+			};
 			pipelineDescriptor.VertexDescriptor = vdesc;
 			var pipeline = device.CreateRenderPipelineState (pipelineDescriptor, error: out error);
 			if (error is not null) {
@@ -197,30 +206,13 @@ namespace CrossGraphics.Metal
 			return null;
 		}
 
-		public void FillPolygon (Polygon poly)
-		{
-			// TODO: Implement
-		}
-
-		public void DrawPolygon (Polygon poly, float w)
-		{
-			// TODO: Implement
-		}
-
-		public void FillRect (float x, float y, float width, float height)
-		{
-			DoRect (x, y, width, height, 0, true);
-		}
-
-		public void DrawRect (float x, float y, float width, float height, float w)
-		{
-			DoRect (x, y, width, height, w, false);
-		}
-
 		public const int VertexPositionByteSize = 2 * sizeof (float);
 		public const int VertexUvByteSize = 2 * sizeof (float);
 		public const int VertexColorByteSize = 4 * sizeof (float);
-		public const int VertexByteSize = VertexPositionByteSize + VertexUvByteSize + VertexColorByteSize;
+		public const int VertexBBByteSize = 4 * sizeof (float);
+		public const int VertexArgsByteSize = 4 * sizeof (float);
+		public const int VertexOpByteSize = 1 * sizeof (uint);
+		public const int VertexByteSize = VertexPositionByteSize + VertexUvByteSize + VertexColorByteSize + VertexBBByteSize + VertexArgsByteSize + VertexOpByteSize;
 
 		public const int UniformModelToViewByteSize = 16 * sizeof (float);
 		public const int UniformByteSize = UniformModelToViewByteSize;
@@ -248,7 +240,7 @@ namespace CrossGraphics.Metal
 				NumVertices = 0;
 				NumIndices = 0;
 			}
-			public int AddVertex (float x, float y, float u, float v, ValueColor color)
+			public int AddVertex (float x, float y, float u, float v, ValueColor color, Vector4 bb, Vector4 args, DrawOp op)
 			{
 				var index = NumVertices;
 				if (VertexBuffer is not null) {
@@ -263,6 +255,15 @@ namespace CrossGraphics.Metal
 						p[5] = color.Green / 255f;
 						p[6] = color.Blue / 255f;
 						p[7] = color.Alpha / 255f;
+						p[8] = bb.X;
+						p[9] = bb.Y;
+						p[10] = bb.Z;
+						p[11] = bb.W;
+						p[12] = args.X;
+						p[13] = args.Y;
+						p[14] = args.Z;
+						p[15] = args.W;
+						*((uint*)(p + 16)) = (uint)op;
 					}
 				}
 				NumVertices++;
@@ -365,46 +366,68 @@ namespace CrossGraphics.Metal
 			}
 		}
 
-		public void DoRect (float x, float y, float width, float height, float w, bool fill)
+		void DoRect (float x, float y, float width, float height, float w, DrawOp op)
 		{
 			var buffer = _buffers.GetBuffer (4, 6);
 			var bb = BoundingBox.FromRect (x, y, width, height, w);
-			var v0 = buffer.AddVertex(bb.MinX, bb.MinY, 0, 0, _currentColor);
-			var v1 = buffer.AddVertex(bb.MaxX, bb.MinY, 1, 0, _currentColor);
-			var v2 = buffer.AddVertex(bb.MaxX, bb.MaxY, 1, 1, _currentColor);
-			var v3 = buffer.AddVertex(bb.MinX, bb.MaxY, 0, 1, _currentColor);
+			var bbv = new Vector4 (bb.MinX, bb.MinY, bb.MaxX, bb.MaxY);
+			var args = new Vector4 (w, 0, 0, 0);
+			var v0 = buffer.AddVertex(bb.MinX, bb.MinY, 0, 0, _currentColor, bb: bbv, args: args, op: op);
+			var v1 = buffer.AddVertex(bb.MaxX, bb.MinY, 1, 0, _currentColor, bb: bbv, args: args, op: op);
+			var v2 = buffer.AddVertex(bb.MaxX, bb.MaxY, 1, 1, _currentColor, bb: bbv, args: args, op: op);
+			var v3 = buffer.AddVertex(bb.MinX, bb.MaxY, 0, 1, _currentColor, bb: bbv, args: args, op: op);
 			buffer.AddTriangle(v0, v1, v2);
 			buffer.AddTriangle(v2, v3, v0);
 		}
 
+		public void FillPolygon (Polygon poly)
+		{
+			// TODO: Implement
+		}
+
+		public void DrawPolygon (Polygon poly, float w)
+		{
+			// TODO: Implement
+		}
+
+		public void FillRect (float x, float y, float width, float height)
+		{
+			DoRect (x, y, width, height, 0, DrawOp.FillRect);
+		}
+
+		public void DrawRect (float x, float y, float width, float height, float w)
+		{
+			DoRect (x, y, width, height, w, DrawOp.StrokeRect);
+		}
+
 		public void FillRoundedRect (float x, float y, float width, float height, float radius)
 		{
-			DoRect (x, y, width, height, 0, true);
+			DoRect (x, y, width, height, 0, DrawOp.FillRoundedRect);
 		}
 
 		public void DrawRoundedRect (float x, float y, float width, float height, float radius, float w)
 		{
-			DoRect (x, y, width, height, w, false);
+			DoRect (x, y, width, height, w, DrawOp.StrokeRoundedRect);
 		}
 
 		public void FillOval (float x, float y, float width, float height)
 		{
-			DoRect (x, y, width, height, 0, true);
+			DoRect (x, y, width, height, 0, DrawOp.FillOval);
 		}
 
 		public void DrawOval (float x, float y, float width, float height, float w)
 		{
-			DoRect (x, y, width, height, w, false);
+			DoRect (x, y, width, height, w, DrawOp.StrokeOval);
 		}
 
 		public void FillArc (float cx, float cy, float radius, float startAngle, float endAngle)
 		{
-			DoRect (cx - radius, cy - radius, radius * 2, radius * 2, 0, true);
+			DoRect (cx - radius, cy - radius, radius * 2, radius * 2, 0, DrawOp.FillArc);
 		}
 
 		public void DrawArc (float cx, float cy, float radius, float startAngle, float endAngle, float w)
 		{
-			DoRect (cx - radius, cy - radius, radius * 2, radius * 2, w, false);
+			DoRect (cx - radius, cy - radius, radius * 2, radius * 2, w, DrawOp.StrokeArc);
 		}
 
 		public void BeginLines (bool rounded)
@@ -424,10 +447,12 @@ namespace CrossGraphics.Metal
 			var nx = dy / len;
 			var ny = -dx / len;
 			var w2 = w / 2;
-			var v0 = buffer.AddVertex (sx - nx * w2, sy - ny * w2, 0, 0, _currentColor);
-			var v1 = buffer.AddVertex (sx + nx * w2, sy + ny * w2, 1, 0, _currentColor);
-			var v2 = buffer.AddVertex (ex + nx * w2, ey + ny * w2, 1, 1, _currentColor);
-			var v3 = buffer.AddVertex (ex - nx * w2, ey - ny * w2, 0, 1, _currentColor);
+			var bbv = new Vector4 (Math.Min (sx, ex) - w2, Math.Min (sy, ey) - w2, Math.Max (sx, ex) + w2, Math.Max (sy, ey) + w2);
+			var args = new Vector4 (w, 0, 0, 0);
+			var v0 = buffer.AddVertex (sx - nx * w2, sy - ny * w2, 0, 0, _currentColor, bb: bbv, args: args, op: DrawOp.DrawLine);
+			var v1 = buffer.AddVertex (sx + nx * w2, sy + ny * w2, 1, 0, _currentColor, bb: bbv, args: args, op: DrawOp.DrawLine);
+			var v2 = buffer.AddVertex (ex + nx * w2, ey + ny * w2, 1, 1, _currentColor, bb: bbv, args: args, op: DrawOp.DrawLine);
+			var v3 = buffer.AddVertex (ex - nx * w2, ey - ny * w2, 0, 1, _currentColor, bb: bbv, args: args, op: DrawOp.DrawLine);
 			buffer.AddTriangle (v0, v1, v2);
 			buffer.AddTriangle (v2, v3, v0);
 		}
@@ -476,10 +501,31 @@ namespace CrossGraphics.Metal
 			_buffers.Reset ();
 		}
 
+		public enum DrawOp
+		{
+			FillRect = 0,
+			StrokeRect = 1,
+			FillRoundedRect = 2,
+			StrokeRoundedRect = 3,
+			FillOval = 4,
+			StrokeOval = 5,
+			FillArc = 6,
+			StrokeArc = 7,
+			FillPolygon = 8,
+			StrokePolygon = 9,
+			FillPath = 10,
+			StrokePath = 11,
+			DrawImage = 12,
+			DrawString = 13,
+			DrawLine = 14,
+		}
+
 		const string MetalCode = @"
+using namespace metal;
+
 typedef struct
 {
-    metal::float4x4 modelViewProjectionMatrix;
+    float4x4 modelViewProjectionMatrix;
 } Uniforms;
 
 typedef struct
@@ -487,6 +533,9 @@ typedef struct
     float2 position [[attribute(0)]];
     float2 texCoord [[attribute(1)]];
 	float4 color [[attribute(2)]];
+	float4 bb [[attribute(3)]];
+	float4 args [[attribute(4)]];
+	uint op [[attribute(5)]];
 } Vertex;
 
 typedef struct
@@ -497,16 +546,49 @@ typedef struct
     float4 color;
 } ColorInOut;
 
+float fillRect(Vertex in)
+{
+	return 1.0;
+}
+
+float strokeRect(Vertex in)
+{
+	float2 p = in.position;
+	float2 size = in.bb.zw - in.bb.xy;
+	float2 halfSize = size / 2;
+	float2 center = in.bb.xy + halfSize;
+	float2 d = abs(p - center) - halfSize + in.args.xy;
+	float2 q = max(d, 0.0);
+	//return 1.0 - step(in.args.x, length(q));
+	return 0.0;
+}
+
 vertex ColorInOut vertexShader(Vertex in [[ stage_in ]],
                                constant Uniforms &uniforms [[ buffer(1) ]])
 {
 	float4 modelPosition = float4(in.position, 0.0, 1.0);
 	float4 projectedPosition = uniforms.modelViewProjectionMatrix * modelPosition;
+    uint op = in.op;
+    float mask = 0.0;
+	switch (op) {
+	case 0: // FillRect
+	case 2: // FillRoundedRect
+	case 4: // FillOval
+	case 6: // FillArc
+		mask = fillRect(in);
+		break;
+	case 14: // DrawLine
+		mask = fillRect(in);
+		break;
+	default:
+		mask = strokeRect(in);
+		break;
+	}
 	ColorInOut out;
 	out.projectedPosition = projectedPosition;
 	out.modelPosition = modelPosition;
 	out.texCoord = in.texCoord;
-	out.color = in.color;
+	out.color = float4(in.color.xyz, in.color.w * mask);
 	return out;
 }
 
